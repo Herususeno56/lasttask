7ce86eee142f5d248ea72a22fd64232f01086b9a
Heru Suseno <herususeno56@gmail.com>
2022-12-01 17:01:51 +0700

    first commit

---
+++ src/pages/v2/Detail/index.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import "./index.scss";
+
+const Detailv2 = () => {
+  const { _id } = useParams();
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_BASEURL2 + _id)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error));
+    return () => {    
+      } 
+  }, [_id]);
+
+  const {id, name, price, stock, status} = product;
+
+  return (
+    <div className="main">
+      <Link to="/v2" className="btn btn-primary">
+        Kembali
+      </Link>
+      <table className="table">
+        <tbody>
+          <tr>
+            <td>ID</td>
+            <td>: {id}</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>: {name}</td>
+          </tr>
+          <tr>
+            <td>Price</td>
+            <td>: Rp{price ? price.toLocaleString("id-ID") : price}</td>
+          </tr>
+          <tr>
+            <td>Stock</td>
+            <td>: {stock}</td>
+          </tr>
+          <tr>
+            <td>Status</td>
+            <td>: {status ? "Active" : "Inactive"}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Detailv2;
